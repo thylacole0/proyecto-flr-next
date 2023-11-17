43840985dbd0caf86f5b42ee0e7df35a4b39bc18
thylacole0 <cesar12zamora@gmail.com>
2023-11-17 01:18:39 -0300

    Update config matcher in middleware.js

--- cliente/middleware.js
+++ cliente/middleware.js
@@ -3,6 +3,7 @@ export { default } from 'next-auth/middleware'
 
 
 export const config = {
-    matcher: ['/home_test', '/form_visitante', '/form_residente', '/form_guardia', '/form_nurse', '/residentes', '/visitantes', '/', '/enfermeros', '/guardias', '/register', '/reserva', '/ficha_residente', '/tablaResEnfer', '/guardiareservas']
+    matcher: ['/home_test', '/form_visitante', '/form_residente', '/form_guardia', '/form_nurse', '/residentes', '/visitantes', '/', 
+    '/enfermeros', '/guardias', '/register', '/reserva', '/ficha_residente', '/tablaResEnfer', '/guardiareservas', '/reservasAprobadas']
 }
 
